ee88ab2e2a4e1e8ea7a9c2435e08980631cdd5a4
OnlyLeafeon <jeddytothelevvy@gmail.com>
2026-04-16 12:59:38 -0600

    Fix dialog text formatting in dialogs.h

--- text/us/dialogs.h
+++ text/us/dialogs.h
@@ -1780,7 +1780,11 @@ Stars! The mystery of the\n\
 endless stairs is solved,\n\
 thanks to you--and is\n\
 Bowser ever upset! Now,\n\
-on to the final bout!"))
+you don't need to\n\
+use the power of your\n\
+Stand the BLG to break\n\
+the laws of physics to\n\
+defeat Bowser."))
 
 DEFINE_DIALOG(DIALOG_147, 1, 5, 30, 200, _("\
 Are you using the Cap\n\
